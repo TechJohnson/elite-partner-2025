35ad378dbb168fbcc07ed61dbe33eec42d707439
Steven Johnson <stevenjohnson92@gmail.com>
2025-05-28 13:49:47 -0400

    Static Menu On Scroll

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 500) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.display = "flex";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
+  }
+}
